c9fff91fb0c99fddaef1ecdce2d6f7ab7ca7446f
Laxman Giri <laxmanvatij@gmail.com>
2025-01-22 17:27:22 +0100

    union&& literal types &&type alias

---
+++ 03_Basics/app.js
@@ -0,0 +1,18 @@
+//union literal types  type alias
+function combine(num1, num2, type) {
+    if (typeof num1 === "number" && typeof num2 === "number" || type === "as-number") {
+        return +num1 + +num2;
+    }
+    else {
+        return num1.toString() + num2.toString();
+    }
+}
+var add = combine("2", "4", "as-number");
+var combinenumber = combine("laxman", "giri", "as string");
+console.log(add, combinenumber);
+var user = {
+    name: "laxman",
+    age: 23,
+    college: "Niels Brock"
+};
+console.log("My name is ".concat(user.name));
---
+++ 03_Basics/app.ts
@@ -0,0 +1,54 @@
+//union literal types  type alias
+
+//union
+//why
+// function combine(num1: number | string, num2: number | string) {
+//   if (typeof num1 === "number" && typeof num2 === "number") {
+//     return num1 + num2;
+//   } else {
+//     return num1.toString() + num2.toString();
+//   }
+// }
+// const add = combine(2, 4);
+// const combinenumber = combine("laxman", "giri");
+// console.log(add, combinenumber);
+
+
+//! literal types
+// function combine(num1: number | string, num2: number | string ,type:"as-number"|"as string") {
+//     if (typeof num1 === "number" && typeof num2 === "number" || type==="as-number") {
+//       return +num1 + +num2;
+//     } else {
+//       return num1.toString() + num2.toString();
+//     }
+//   }
+//   const add = combine("2", "4","as-number");
+//   const combinenumber = combine("laxman", "giri","as string");
+//   console.log(add, combinenumber);
+
+//type alias /custom types
+type Combinable=number|string
+type combined="as-number"|"as string"
+function combine(num1: Combinable, num2: Combinable ,type:combined) {
+    if (typeof num1 === "number" && typeof num2 === "number" || type==="as-number") {
+      return +num1 + +num2;
+    } else {
+      return num1.toString() + num2.toString();
+    }
+  }
+  const add = combine("2", "4","as-number");
+  const combinenumber = combine("laxman", "giri","as string");
+  console.log(add, combinenumber);
+
+type User={
+    name:String,
+    age:number
+    college:"Niels Brock"
+}
+
+const user:User={
+    name:"laxman",
+    age:23,
+    college:"Niels Brock"
+}
+console.log(`My name is ${user.name}`);
