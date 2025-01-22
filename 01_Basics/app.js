76085cd429f509306382244d58c90aa1929cc67f
Laxman Giri <laxmanvatij@gmail.com>
2025-01-22 15:56:14 +0100

    typescrip types

---
+++ 01_Basics/app.js
@@ -0,0 +1,17 @@
+function add(number1, number2, printResult, someText) {
+    if (printResult) {
+        console.log("".concat(someText) + number1 + number2);
+    }
+    else {
+        console.log("Print is false");
+    }
+}
+var n1 = 30;
+var n2 = 30;
+var printResult = true;
+var someText = "The result is :";
+add(n1, n2, printResult, someText);
+function multiply(n1, n2) {
+    return n1 * n2;
+}
+console.log(multiply(4, "4"));
---
+++ 01_Basics/app.ts
@@ -0,0 +1,23 @@
+function add(
+  number1: number,
+  number2: number,
+  printResult: boolean,
+  someText: String
+) {
+  if (printResult) {
+    console.log(`${someText}` + number1 + number2);
+  } else {
+    console.log("Print is false");
+  }
+}
+const n1 = 30;
+const n2 = 30;
+const printResult = true;
+const someText = "The result is :";
+add(n1, n2, printResult, someText);
+
+function multiply(n1:number,n2:number){
+    return n1*n2
+}
+console.log(multiply(4,4));
+
